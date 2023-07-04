36aeee621be81d9d24a793cdd37e5226d5c9870e
Christopher-Deo <chris@christopherdeo.com>
2023-07-04 13:28:04 -0500

    initial project file setup

---
+++ src/main/java/com/codewhisperer/marketlinx/MarketlinxApplication.java
@@ -0,0 +1,14 @@
+package com.codewhisperer.marketlinx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketlinxApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(MarketlinxApplication.class, args);
+	}
+//class methods here
+}
---
+++ src/test/java/com/codewhisperer/marketlinx/MarketlinxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codewhisperer.marketlinx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketlinxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
